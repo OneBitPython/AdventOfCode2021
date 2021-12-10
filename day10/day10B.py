9383023e2a85426e0b1007f4815497c795300001
ananthram <ananthramtennis12@gmail.com>
2021-12-10 12:08:31 +0530

    commited day 10

---
+++ day10/day10B.py
@@ -0,0 +1,28 @@
+arr = []
+inverse = {">": "<", "]": "[", ")": "(", "}": "{"}
+score = {"(":1,"[":2, "{":3,"<":4}
+t = []
+incomplete = []
+for line in open("C:\\Users\\anant\\PythonProjects\\AdventOfCode\\day10\\inp10.txt").readlines():
+    stack = []
+    ans = 0
+    line = line.strip()
+    incorrect = ""
+    for value in line:
+        if value in inverse.values():
+            stack.append(value)
+        elif value in inverse.keys():
+            if len(stack) > 0 and inverse[value] == stack[-1]:
+                stack.pop() 
+            else:
+                incorrect = value
+                break
+    if incorrect=="": 
+        stack.reverse()                
+        for i in stack:
+            ans*=5
+            ans+=score[i]
+        t.append(ans)
+
+t.sort()
+print(t[len(t)//2])
