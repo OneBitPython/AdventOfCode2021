50f9e980502d46f67180e3d4c0dd27dfe14abc95
ananthram <ananthramtennis12@gmail.com>
2021-12-12 13:14:27 +0530

    Commited day 12

---
+++ day12/part2.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+
+neighbours = defaultdict(lambda : [])
+for line in open("C:\\Users\\anant\\PythonProjects\\AdventOfCode\\day12\\inp12.txt"):
+    line = line.strip().split("-")
+    neighbours[line[0]].append(line[1])
+    neighbours[line[1]].append(line[0])
+
+queue = [("start",set(["start"]),False)]
+ans = 0
+while queue:
+    pos,small_caves,mode = queue.pop()
+    if pos == 'end':
+        ans+=1
+    else:
+        for neighbour in neighbours[pos]:
+            if neighbour not in small_caves:
+                new_small_caves = set(small_caves)
+                if neighbour.islower():
+                    new_small_caves.add(neighbour)
+                queue.append((neighbour,new_small_caves, mode))
+            else:
+                if mode == False and neighbour not in ["start", "end"]:
+                    queue.append((neighbour, small_caves, True))
+print(ans)
